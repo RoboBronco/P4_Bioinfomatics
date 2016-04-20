f1f82e3e340e514d8b3863f601cc5d4eb0ce5e70
jayden2013 <jay-weav@hotmail.com>
2016-04-20 17:16:17 -0600

    Now parses one line sequence at a time, and returns it...on demand.
    Updated driver as well to test and reflect usage.

--- Parser.java
+++ Parser.java
@@ -14,54 +14,55 @@ public class Parser extends ConvertDNAToLong {
 	private long longRepresentation;
 	private Scanner scan;
 	private Collection<Character> dnaSeq = new ArrayList<Character>();
+	private String current = "";
 
 	/**
 	 * Attempts to open a file with a scanner and then calls the parseIt method.
 	 * @param file
 	 */
-	public void parse(File file){
-		try {
-			this.scan = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			System.out.println("lel. file not found.");
+	public void parseNext(File file){
+		if (current == ""){ //don't want to make a scanner again each time lol.
+			try {
+				this.scan = new Scanner(file);
+			} catch (FileNotFoundException e) {
+				System.out.println("lel. file not found.");
+			}
 		}
 		parseIt();		
 	}
-	
+
 	/**
 	 * Does the actual parsing of the dna strings and calls the convertToLong method, inside of the ConvertDNAToLong class.
 	 */
-	private void parseIt(){
-		String current = "";
+	private Collection<Character> parseIt(){
+
 		//get to the DNA part...
-		while(!current.equals("ORIGIN      ")){
-			current = scan.nextLine();
+		if (current == ""){
+			while(!current.equals("ORIGIN      ")){
+				current = scan.nextLine();
+			}
 		}
+		dnaSeq.clear();
 		//get dna
 		char currentChar = ' ';
 		while (!current.equals("//")){
 			current = scan.next();
+
 			for (int k = 0; k < current.length(); k++){
 				currentChar = current.charAt(k);
 				if (currentChar > 64){ //To exclude numbers from being added to collection, ascii table values. if we don't exclude numbers....convertToLong will fail.
 					dnaSeq.add(currentChar);
 				}
-
 			}
-			this.longRepresentation = convertToLong(dnaSeq); //convert to long.
-			//For Testing
-			System.out.println(longRepresentation); //print the long.
-			System.out.println(convertFromLong(this.longRepresentation)); //confirm we get what we expect back.
+			break;
+		}
+		return dnaSeq;
 
-			//Clear the collection.
-			dnaSeq.clear();
-		}
-		
-		//multiple dna set capabilities
-		if (scan.hasNext()){
-			parseIt();
-		}
-		
+		//		//multiple dna set capabilities
+		//		if (scan.hasNext()){
+		//			parseIt();
+		//		}
+
 	}
 
 	/**
@@ -70,6 +71,14 @@ public class Parser extends ConvertDNAToLong {
 	 */
 	public long getLongRepresentation(){
 		return this.longRepresentation;
+	}
+
+	/**
+	 * Returns the DNA Sequence Collection.
+	 * @return DNA Sequence
+	 */
+	public Collection<Character> DNASequence(){
+		return dnaSeq;
 	}
 
 }
--- driver.java
+++ driver.java
@@ -1,12 +1,15 @@
+
 import java.io.File;
 
 public class driver {
 	public static void main(String[] args){
 		Parser parse = new Parser();
 		File file = new File(args[0]);
-		parse.parse(file);
-		
-		
+		for(int i = 0; i < 100; i++){
+			parse.parseNext(file);
+			System.out.println(parse.DNASequence());
+			System.out.println(parse.convertToLong(parse.DNASequence()));
+		}
 	}
 
 }
