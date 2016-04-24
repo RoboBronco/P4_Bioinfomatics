a5e5373e480bd449ffd760c230ade55ba1dd4b51
Prince Kannah <pkannah@zdatainc.com>
2016-04-24 14:22:06 -0600

    merged

--- src/OptimalDegreeGenerator.java
+++ src/OptimalDegreeGenerator.java
@@ -1,15 +1,15 @@
-
-public class OptimalDegreeGenerator{
-	
-	public static int generate() {
-		
-		int dbSize = 4096;
-        int nodeSize = 50; //TODO find a more accurate size
-        int pointerSize = 4; //TODO find a more accurate size
-        int metadata = 100;
-        //value for degree will be truncated, which is okay, as optimal degree must be <= actual value
-
-		int degree = (dbSize - 100) / (2*nodeSize + 2*pointer)	
-		return degree;
-	}
-}
+//
+//public class OptimalDegreeGenerator{
+//
+//	public static int generate() {
+//
+//		int dbSize = 4096;
+//        int nodeSize = 50; //TODO find a more accurate size
+//        int pointerSize = 4; //TODO find a more accurate size
+//        int metadata = 100;
+//        //value for degree will be truncated, which is okay, as optimal degree must be <= actual value
+//
+//		int degree = (dbSize - 100) / (2*nodeSize + 2*pointer)
+//		return degree;
+//	}
+//}
