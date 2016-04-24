483a57bfbfc14a1b39084c95c19c396961725632
EYan <Iangilman@u.boisestate.edu>
2016-04-24 17:03:28 -0600

    Updated GeneBankCreateTree again

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -37,7 +37,11 @@ public class GeneBankCreateBTree {
 		int debug = 0;
 		
 		if (args.length == 5){ 
-			debug = args[4];
+			 int temp = Integer.parseInt[4];
+            if (temp == 1){
+                debug = 1;
+            }
+
 		}
 
 		if (degree == 0){
@@ -60,13 +64,15 @@ public class GeneBankCreateBTree {
             exit(1);
 		}
 
+		//create BTree here
+		BTree tree = new BTree(degree, sequence);
+
 		try {
 			File gbFile = new File(gbName); // TODO double check that this is working. 
 			//here is where parsing takes place 
 			//Each sequence will be passed to BTree as a new BTree Object, and handled there
-			//Once we have finished parsing the file, we will write the BTree to disc
-			//The file containing the BTree will be named according to the project specifications. 
-			//I may write a method or seperate class to handle naming.
+						
+		
 		}
 		catch(FileNotFoundException fnfe){
 			system.out.println("Genebank File Not Found: Program Terminated.");
@@ -81,8 +87,46 @@ public class GeneBankCreateBTree {
 	public static <T> void cachedBTree(){
 	
 	//This will, more or less, be the same as the above. Will update once we've discussed cache implementation.	
-	
-	
+	 	int degree = Integer.parseInt(args[1]);
+        String gbName = args[2];
+        int sequence = Integer.parseInt(args[3]);
+        int cacheSize = Integer.parseInt(args[4]); 
+		int debug = 0;
+
+        if (args.length == 6){
+        	int temp = Integer.parseInt[5];
+			if (temp == 1){
+				debug = 1;
+			}
+		}
+
+        if (degree == 0){
+        //generate opitmal degree
+        system.out.println("Generating optimal degree based on disc size");
+        degree = OptimalDegreeGenerator.generate();
+        }
+
+        if (!sequence >= 1){
+            System.out.println("specified sequence is too short, or is negative");
+            System.out.println("Please specifiy a sequence length such that 1 <= sequence <= 31")
+            System.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
+            exit(1);
+        }
+
+        if (!sequence <= 31){
+            System.out.println("specified sequence length is too long.")
+             System.out.println("Please specifiy a sequence length such that 1 <= sequence <= 31")
+            System.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
+            exit(1);
+        }
+
+        //create BTree here
+        BTree tree = new BTree(degree, sequence);
+
+        try {
+            File gbFile = new File(gbName); // TODO double check that this is working.
+            //here is where parsing takes place
+            //Each sequence will be passed to BTree as a new BTree Object, and handled there	
 	}
 						
 }
