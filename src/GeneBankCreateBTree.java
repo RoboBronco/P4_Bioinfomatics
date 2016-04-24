9fea1537d6ba47a9418359e998ce3d5c73099055
Prince Kannah <pkannah@zdatainc.com>
2016-04-24 12:22:49 -0600

    testing TreeWriter Object

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -1,31 +1,31 @@
-public class GeneBankCreateBTree {
-    public static void main (String[] args){
-
-
-            if (args.length() != 4 || args.length() != 3 ){
-            //print usage
-                system.out.println("Usage: <><><>[<>]");
-            //exit
-            }
-
-            else{
-              int degree;
-
-              if (args[0] == 0){
-                //generate optimal degree
-                //degree = whatever is returned
-              }
-              else{
-                degree = Math.abs(args[1]);
-              }
-
-              try{
-              File gbk = new File()
-              }
-
-            }
-
-
-          } //end main
-} //end class
-
+//public class GeneBankCreateBTree {
+//    public static void main (String[] args){
+//
+//
+//            if (args.length() != 4 || args.length() != 3 ){
+//            //print usage
+//                system.out.println("Usage: <><><>[<>]");
+//            //exit
+//            }
+//
+//            else{
+//              int degree;
+//
+//              if (args[0] == 0){
+//                //generate optimal degree
+//                //degree = whatever is returned
+//              }
+//              else{
+//                degree = Math.abs(args[1]);
+//              }
+//
+//              try{
+//              File gbk = new File()
+//              }
+//
+//            }
+//
+//
+//          } //end main
+//} //end class
+//
