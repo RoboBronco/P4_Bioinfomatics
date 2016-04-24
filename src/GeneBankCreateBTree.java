36b2fd267f8bb70f76b51eddfd9f239e633f5574
Prince Kannah <PrinceKannah@u.boisestate.edu>
2016-04-24 14:23:31 -0600

    Prince (#17)
    * project clean up
    
    * created TreeWriter
    
    * testing TreeWriter Object
    
    * updated treewriter
    
    * merged

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -1,77 +1,111 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-
-public class GeneBankCreateBTree {
-    public static void main (String[] args){
-
-
-            if (args.length() < 4 || args.length() > 6 ){
-            //print usage
-                system.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
-				system.out.println("[<cache size>] should only be specified if you are using a cache.");
-            //exit
-            }
-     		else{
-				if (args[0] == 0){
-					cachelessBTree();
-				}
-				else if (args[0] == 1){
-					cachedBTree();
-				}
-				else{
-				system.out.println("First argument must be a 0 or 1 to determine if cache is used");
-				system.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
-				system.out.println("[<cache size>] should only be specified if you are using a cache.");
-				}
-
-            }
-	}
-
-		
-	public static <T> void cachelessBTree(){ // generic? static necessary?
-		
-		int degree = Integer.parseInt(args[1]);
-		String gbName = args[2];
-		int sequence = Integer.parseInt(args[3]);
-		int debug = 0;
-		
-		if (args.length == 5){ 
-			debug = args[4];
-		}
-
-		if (degree == 0){
-		//generate opitmal degree
-		system.out.println("Generating optimal degree based on disc size"); 
-		degree = OptimalDegreeGenerator.generate();		
-		}
-		
-		if (!sequence <= 31 || !sequence >= 1){
-		//TODO error handling for improper sequence length values 	
-		}
-
-		try {
-			File gbFile = new File(gbName); // TODO double check that this is working. 
-			//here is where parsing takes place 
-			//Each sequence will be passed to BTree as a new BTree Object, and handled there
-			//Once we have finished parsing the file, we will write the BTree to disc
-			//The file containing the BTree will be named according to the project specifications. 
-			//I may write a method or seperate class to handle naming.
-		}
-		catch(FileNotFoundException fnfe){
-			system.out.println("Genebank File Not Found: Program Terminated.");
-			exit(1);
-		}	
-	
-	}
-
-
-
-
-	public static <T> void cachedBTree(){
-	
-	//This will, more or less, be the same as the above. Will update once we've discussed cache implementation.	
-	
-	
-	}
-						
-}
+//<<<<<<< HEAD
+////public class GeneBankCreateBTree {
+////    public static void main (String[] args){
+////
+////
+////            if (args.length() != 4 || args.length() != 3 ){
+////            //print usage
+////                system.out.println("Usage: <><><>[<>]");
+////            //exit
+////            }
+////
+////            else{
+////              int degree;
+////
+////              if (args[0] == 0){
+////                //generate optimal degree
+////                //degree = whatever is returned
+////              }
+////              else{
+////                degree = Math.abs(args[1]);
+////              }
+////
+////              try{
+////              File gbk = new File()
+////              }
+////
+////            }
+////
+////
+////          } //end main
+////} //end class
+////
+//=======
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//
+//public class GeneBankCreateBTree {
+//    public static void main (String[] args){
+//
+//
+//            if (args.length() < 4 || args.length() > 6 ){
+//            //print usage
+//                system.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
+//				system.out.println("[<cache size>] should only be specified if you are using a cache.");
+//            //exit
+//            }
+//     		else{
+//				if (args[0] == 0){
+//					cachelessBTree();
+//				}
+//				else if (args[0] == 1){
+//					cachedBTree();
+//				}
+//				else{
+//				system.out.println("First argument must be a 0 or 1 to determine if cache is used");
+//				system.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
+//				system.out.println("[<cache size>] should only be specified if you are using a cache.");
+//				}
+//
+//            }
+//	}
+//
+//
+//	public static <T> void cachelessBTree(){ // generic? static necessary?
+//
+//		int degree = Integer.parseInt(args[1]);
+//		String gbName = args[2];
+//		int sequence = Integer.parseInt(args[3]);
+//		int debug = 0;
+//
+//		if (args.length == 5){
+//			debug = args[4];
+//		}
+//
+//		if (degree == 0){
+//		//generate opitmal degree
+//		system.out.println("Generating optimal degree based on disc size");
+//		degree = OptimalDegreeGenerator.generate();
+//		}
+//
+//		if (!sequence <= 31 || !sequence >= 1){
+//		//TODO error handling for improper sequence length values
+//		}
+//
+//		try {
+//			File gbFile = new File(gbName); // TODO double check that this is working.
+//			//here is where parsing takes place
+//			//Each sequence will be passed to BTree as a new BTree Object, and handled there
+//			//Once we have finished parsing the file, we will write the BTree to disc
+//			//The file containing the BTree will be named according to the project specifications.
+//			//I may write a method or seperate class to handle naming.
+//		}
+//		catch(FileNotFoundException fnfe){
+//			system.out.println("Genebank File Not Found: Program Terminated.");
+//			exit(1);
+//		}
+//
+//	}
+//
+//
+//
+//
+//	public static <T> void cachedBTree(){
+//
+//	//This will, more or less, be the same as the above. Will update once we've discussed cache implementation.
+//
+//
+//	}
+//
+//}
+//>>>>>>> c1207b08e418787f43e417f3d6971c335c3c65fc
