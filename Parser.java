39ad00311f40f663476875060352977c4e357e83
jayden2013 <jay-weav@hotmail.com>
2016-04-25 14:25:51 -0600

    oops.

--- Parser.java
+++ Parser.java
@@ -14,55 +14,54 @@ public class Parser extends ConvertDNAToLong {
 	private long longRepresentation;
 	private Scanner scan;
 	private Collection<Character> dnaSeq = new ArrayList<Character>();
-	private String current = "";
 
 	/**
 	 * Attempts to open a file with a scanner and then calls the parseIt method.
 	 * @param file
 	 */
-	public void parseNext(File file){
-		if (current == ""){ //don't want to make a scanner again each time lol.
-			try {
-				this.scan = new Scanner(file);
-			} catch (FileNotFoundException e) {
-				System.out.println("lel. file not found.");
-			}
+	public void parse(File file){
+		try {
+			this.scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("lel. file not found.");
 		}
 		parseIt();		
 	}
-
+	
 	/**
 	 * Does the actual parsing of the dna strings and calls the convertToLong method, inside of the ConvertDNAToLong class.
 	 */
-	private Collection<Character> parseIt(){
-
+	private void parseIt(){
+		String current = "";
 		//get to the DNA part...
-		if (current == ""){
-			while(!current.equals("ORIGIN      ")){
-				current = scan.nextLine();
-			}
+		while(!current.equals("ORIGIN      ")){
+			current = scan.nextLine();
 		}
-		dnaSeq.clear();
 		//get dna
 		char currentChar = ' ';
 		while (!current.equals("//")){
 			current = scan.next();
-
 			for (int k = 0; k < current.length(); k++){
 				currentChar = current.charAt(k);
 				if (currentChar > 64){ //To exclude numbers from being added to collection, ascii table values. if we don't exclude numbers....convertToLong will fail.
 					dnaSeq.add(currentChar);
 				}
+
 			}
-			break;
+			this.longRepresentation = convertToLong(dnaSeq); //convert to long.
+			//For Testing
+			System.out.println(longRepresentation); //print the long.
+			System.out.println(convertFromLong(this.longRepresentation)); //confirm we get what we expect back.
+
+			//Clear the collection.
+			dnaSeq.clear();
 		}
-		return dnaSeq;
-
-		//		//multiple dna set capabilities
-		//		if (scan.hasNext()){
-		//			parseIt();
-		//		}
-
+		
+		//multiple dna set capabilities
+		if (scan.hasNext()){
+			parseIt();
+		}
+		
 	}
 
 	/**
@@ -71,14 +70,6 @@ public class Parser extends ConvertDNAToLong {
 	 */
 	public long getLongRepresentation(){
 		return this.longRepresentation;
-	}
-
-	/**
-	 * Returns the DNA Sequence Collection.
-	 * @return DNA Sequence
-	 */
-	public Collection<Character> DNASequence(){
-		return dnaSeq;
 	}
 
 }
