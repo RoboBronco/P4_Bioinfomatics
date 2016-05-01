d29d75573b79912e32393b079345a75d496ec286
jholden1b <joshuaholden@u.boisestate.edu>
2016-05-01 16:07:32 -0600

    Added files via upload

--- src/GeneBankSearch.java
+++ src/GeneBankSearch.java
@@ -1,14 +1,18 @@
 import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class GeneBankSearch 
 {
 	private static BTree btree;
 	private static int t;
 	private static int k;
-	private static int debug;	
+	private static int debug = 0;	
 	private static int cacheSize;
+	
 	// args[1]
 	private static String btreeFile;
+	
 	// args[2]
 	private static String queryFile;
 	
@@ -23,17 +27,35 @@ public class GeneBankSearch
 			System.exit(1);
 		}
 
+		
 		if((Integer.parseInt(args[0]) == 0)){
 			btreeFile = args[1];
 			queryFile = args[2];
-			k = Integer.parseInt(btreeFile.substring(btreeFile.length() - 1));
-			t = Integer.parseInt(btreeFile.substring(btreeFile.length() - 3));
+
+			StringTokenizer st = new StringTokenizer(btreeFile, ".");
+			int count = st.countTokens();
+			String tokens[]= new String[count];
+			int j = 0;
+			while(j <= count){
+				while(st.hasMoreElements()) {
+					tokens[j] = st.nextToken();
+					j++;
+				}
+			j++;
+			}
+			
+			k = Integer.parseInt(tokens[count - 2]);
+			t = Integer.parseInt(tokens[count - 1]);
+			
+			System.out.println("k: " + k + " t: " + t  );
+			
 			if((args.length == 4)){
 				debug = Integer.parseInt(args[3]);
 			}
 		}else if((Integer.parseInt(args[0]) == 1)){
 			btreeFile = args[1];
 			queryFile = args[2];
+			
 			if(args.length == 4){
 				cacheSize = Integer.parseInt(args[3]);
 			}
@@ -47,19 +69,20 @@ public class GeneBankSearch
 		}
 
 		String queryInput;
-		BufferedReader reader;
+		Scanner scan;
+		
 		try
 		{
-			btree = new BTree(new File(btreeFile), t, k);
-			reader = new BufferedReader(new FileReader(queryFile));
+			btree = new BTree(new File(btreeFile), 2, 5, 0);
+			scan = new Scanner(new FileReader(queryFile));
 
-			while((queryInput = reader.readLine()) != null) 
+			while((queryInput = scan.nextLine()) != null) 
 			{
 				queryInput = queryInput.toUpperCase();
 				
 				if (debug == 0){
 					search = new TreeObject(ConvertDNAToLong.convertToLong(queryInput));
-					System.out.println(btree.search(search) + queryInput);
+					btree.search(search);
 				}
 				else{ 
 					search = new TreeObject(ConvertDNAToLong.convertToLong(queryInput));
@@ -88,4 +111,5 @@ public class GeneBankSearch
 	{
 		System.out.println("Usage...");
 	}
-}
+	
+	}
