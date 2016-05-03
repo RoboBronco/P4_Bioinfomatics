e42804f5b40d750c18b926e416f01230ee457d1b
Prince Kannah <PrinceKannah@u.boisestate.edu>
2016-05-02 19:12:41 -0600

    Implemented cache

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import static java.lang.System.exit;
 
 public class GeneBankCreateBTree {
     public static void main (String[] args){
@@ -8,7 +9,7 @@ public class GeneBankCreateBTree {
             //print usage
             System.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
             System.out.println("[<cache size>] should only be specified if you are using a cache.");
-            System.exit(1);
+            exit(1);
         }
         else{
             if (Integer.parseInt(args[0]) == 0){
@@ -21,15 +22,16 @@ public class GeneBankCreateBTree {
                 System.out.println("First argument must be a 0 or 1 to determine if cache is used");
                 System.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
                 System.out.println("[<cache size>] should only be specified if you are using a cache.");
-                System.exit(1);
+                exit(1);
             }
 
         }
     }
 
-    public static <T> void cachelessBTree(String[] args) {
+    public static void cachelessBTree(String[] args) {
 
         int degree = Integer.parseInt(args[1]);
+
         String gbName = args[2];
         int sequence = Integer.parseInt(args[3]);
         int debug = 0;
@@ -49,14 +51,14 @@ public class GeneBankCreateBTree {
             System.out.println("specified sequence is too short, or is negative");
             System.out.println("Please specifiy a sequence length such that 1 <= sequence <= 31");
             System.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
-            System.exit(1);
+            exit(1);
         }
 
         if (sequence > 31){
             System.out.println("specified sequence length is too long.");
             System.out.println("Please specifiy a sequence length such that 1 <= sequence <= 31");
             System.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
-            System.exit(1);
+            exit(1);
         }
 
         try {
@@ -64,7 +66,7 @@ public class GeneBankCreateBTree {
             File gbFile = new File(gbName);
             ArrayList<Long> dna = p.parse(gbFile, sequence);
 
-            BTree tree = new BTree(degree, sequence, genFileName, debug);
+            BTree tree = new BTree(degree, sequence, genFileName, debug, 0);
 
 
             //Each sequence will be passed to BTree as a new BTree Object, and handled there
@@ -76,53 +78,49 @@ public class GeneBankCreateBTree {
             tree.writeTree();
             if (debug == 1)
                 tree.writeDumpFile();
-        } catch(IOException e){
+        } catch(IOException e) {
             System.err.println("Genebank File Not Found: Program Terminated.");
-            System.exit(1);
+            exit(1);
         }
 
     }
 
-    public static <T> void cachedBTree(String[] args) {
-
-        //This will, more or less, be the same as the above. Will update once we've discussed cache implementation.
-
-        int degree = Integer.parseInt(args[1]);
-        String gbName = args[2];
-        int sequence = Integer.parseInt(args[3]);
-        int cacheSize = Integer.parseInt(args[4]);
+    public static void cachedBTree(String[] args) {
+        int cacheSize = 0;
         int debug = 0;
 
-        /*if (args.length == 6){
-        	int temp = Integer.parseInt[5];
-			if (temp == 1){
-				debug = 1;
-			}
-		}
+        if (Integer.parseInt(args[0]) == 1)
+            cacheSize = Integer.parseInt(args[4]);
+        if(Integer.parseInt(args[5]) == 1)
+            debug = 1;
 
-        if (!sequence >= 1){
-            System.out.println("specified sequence is too short, or is negative");
-            System.out.println("Please specifiy a sequence length such that 1 <= sequence <= 31")
-            System.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
-            exit(1);
-        }
+        int degree = Integer.parseInt(args[1]);
+        String gbkFile = args[2];
+        int sequenceLength = Integer.parseInt(args[3]);
+        String genFileName = gbkFile+ ".btree.data." + sequenceLength + "." + degree;
 
-        if (!sequence <= 31){
-            System.out.println("specified sequence length is too long.")
-             System.out.println("Please specifiy a sequence length such that 1 <= sequence <= 31")
-            System.out.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
-            exit(1);
-        }
-
-        BTree tree = new BTree(degree, sequence);
 
         try {
-            File gbFile = new File(gbName);
-            //here is where parsing takes place
+            Parser p = new Parser();
+            File gbFile = new File(gbkFile);
+            ArrayList<Long> dna = p.parse(gbFile, sequenceLength);
 
-           	//Each sequence will be passed to BTree as a new BTree Object, and handled there
-			//TODO debug level 1
-		*/
+            BTree tree = new BTree(degree, sequenceLength, genFileName, debug, cacheSize);
 
+
+            //Each sequence will be passed to BTree as a new BTree Object, and handled there
+            int noSeqs = dna.size();
+            for(int i = 0; i < noSeqs; i++) {
+                TreeObject obj = new TreeObject(dna.get(i));
+                tree.insert(obj);
+            }
+            tree.writeTree();
+            if (debug == 1)
+                tree.writeDumpFile();
+        } catch(IOException e) {
+            System.err.println("Genebank File Not Found: Program Terminated.");
+            exit(1);
+        }
     }
+
 }
